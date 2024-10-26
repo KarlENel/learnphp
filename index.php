647630ff8695fbfa9317730e13f47c91df787161
Karl Erik Nelson <karlerik.nelson@tptlive.ee>
2024-10-26 11:05:28 +0300

    numbers

--- index.php
+++ index.php
@@ -2,6 +2,14 @@
 
 $num = 1;
 $num = 2.3;
+$num = 1 + 2;
+$num = 1 - 2;
+$num = 1 * 2;
+$num = 1 / 2;
+$num = $num + 4;
+$num += 4;
+$num = 2**3;
+$num %= 3;
 $string = 'Hello world';
 $string = 'õ';
 $bool = true;
@@ -11,4 +19,4 @@ $var;
 $array = ['cherry', 'apple', 'pear', 1, null, false, true];
 $array = array('cherry', 'apple', 'pear', 1, null, false, true);
 $object = new stdClass();
-var_dump($object); 
+var_dump($num); 
